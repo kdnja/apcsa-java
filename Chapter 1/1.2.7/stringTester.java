29a8b5925ebe00b688b0d7f2061f6f07f2968823
Caden Jamason <cadenjamason@gmail.com>
2021-12-07 12:55:00 -0800

    added 1.2.7

---
+++ Chapter 1/1.2.7/stringTester.java
@@ -0,0 +1,12 @@
+public class stringTester {
+  public static void main(String[] args) {
+    String a = "horse";
+    String b = "hor";
+
+    // playing around with String methods
+    System.out.println(a); // prints a
+    System.out.println(b); // prints b
+    System.out.println(a.compareTo(b)); // prints lexigraphic difference between a and b
+    System.out.println(b.compareTo(a)); // prints lexigraphic difference between b and a
+  }
+}
