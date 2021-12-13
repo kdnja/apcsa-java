f0c40ed79b0c76d24516db3d6c5b385e673d2d50
Caden Jamason <cadenjamason@gmail.com>
2021-12-13 12:36:26 -0800

    1.2.9, 1.3.1

---
+++ Chapter 1/1.2.9/Annuity.java
@@ -0,0 +1,7 @@
+public class Annuity {
+  public double getPresentValue(int pmt, int i, int n) {
+    double rate = (double) i / 100;
+    double pv = pmt * (1 - Math.pow((1 + r), -n)) / r;
+    return pv;
+  }
+}
