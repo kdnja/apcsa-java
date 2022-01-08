680a350c6c1482044de04d17c25a751fd0092122
Caden Jamason <me@kdn.pw>
2022-01-08 13:37:53 -0800

    Update SumAndAverageAndMode.java

--- Chapter 2/2.4.1/SumAndAverageAndMode.java
+++ Chapter 2/2.4.1/SumAndAverageAndMode.java
@@ -1,7 +1,13 @@
+
+/*
+ * Activity 2.4.1
+ */
+
 import java.util.Scanner;
 
 public class SumAndAverageAndMode {
     public static void main(String[] args) {
-    
+        Scanner sc = new Scanner(System.in);
+        
     }
 }
