240ea75883e13db9370fd6b9463ffc28f2b24aca
Caden Jamason <me@kdn.pw>
2022-01-25 20:26:19 -0800

    first 2.5.7 commit

---
+++ Chapter 2/2.5.7/MediaLibrary/MediaLibRunner.java
@@ -0,0 +1,12 @@
+/*
+ * Activity 2.5.7
+ * 
+ * The runner for the MediaLibrary program
+ */
+public class MediaLibRunner {
+    public static void main(String[] args) {
+        System.out.println("Welcome to your Media Library");
+        MediaLib myLib = new MediaLib();
+
+    }
+}
