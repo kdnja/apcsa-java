35d085a5f21641ddb321e0c4a3097f0dbe2416a5
Caden Jamason <me@kdn.pw>
2022-01-26 11:15:10 -0800

    [01/26/2022]: steps 1-4

--- Chapter 2/2.5.7/MediaLibrary/MediaLibRunner.java
+++ Chapter 2/2.5.7/MediaLibrary/MediaLibRunner.java
@@ -6,7 +6,18 @@
 public class MediaLibRunner {
     public static void main(String[] args) {
         System.out.println("Welcome to your Media Library");
+        // creates a new media library
         MediaLib myLib = new MediaLib();
+        // lists books in library before books are added
+        System.out.println("Library:\n" + myLib);
+        // creates a new book
+        Book myBook = new Book("The Lord of the Rings", "Tolkien");
+        // prints out confirmation of book creation
+        System.out.println("Book created " + myBook);
+        // adds book to media library
+        myLib.addBook(myBook);
+        // lists books in library after books are added
+        // System.out.println("Library:\n" + myLib);
 
     }
 }
